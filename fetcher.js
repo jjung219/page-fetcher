68748c634a31c0a81e6f04c7c37ecfb1eed18382
Jennifer Jung <jenjung2194@gmail.com>
2020-11-05 18:02:26 +0000

    page-fetcher function

--- fetcher.js
+++ fetcher.js
@@ -4,19 +4,31 @@ const fs = require('fs');
 const filename = args[1];
 const url = args[0];
 
+
 request(url, (error, response, body) => {
   //how to get the filesize - https://stackoverflow.com/questions/42363140/how-to-find-the-size-of-the-file-in-node-js
   let fileSize = fs.statSync(filename);
   let fileSizeBytes = fileSize['size'];
 
-
-  // how to write file - https://nodejs.org/api/fs.html#fs_fs_writefile_file_data_options_callback
-  fs.writeFile(filename, body, (err) => {
-    if (err) {
-      console.log("Could not read the file");
-      console.log("Error: ", err);
-    }
-    console.log(`Downloaded and saved ${fileSizeBytes} bytes to ${filename}`);
-  });
+  // console.log('Error: ', response.statusCode, response.statusMessage)
+  if (error === null && response.statusCode < 400) {
+    fs.stat(filename, function (err, stat) {
+      if (err == null) {
+        fs.writeFile(filename, body, (err) => {
+          console.log(`Downloaded and saved ${fileSizeBytes} bytes to ${filename}`);
+        });
+      } else if (err.code === 'ENOENT') {
+        console.log('Error: Filepath you provided did not exist')
+      } else {
+        console.log('Some other error: ', err.code);
+      }
+    });
+  } else {
+    console.log('Error requesting a page:', response.statusCode, response.statusMessage)
+  }
 });
 
+
+
+// how to validate filepath - https://stackoverflow.com/questions/17699599/node-js-check-if-file-exists
+// how to write file - https://nodejs.org/api/fs.html#fs_fs_writefile_file_data_options_callback$
